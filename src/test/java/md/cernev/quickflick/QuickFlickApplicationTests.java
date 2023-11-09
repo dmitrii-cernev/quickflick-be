75838d8104647a615b5d2dc83e00f04c4b63e1e8
Dmitrii Cernev <ext-dmitrii.cernev@here.com>
2023-11-09 20:18:57 +0100

    init commit

---
+++ src/test/java/md/cernev/quickflick/QuickFlickApplicationTests.java
@@ -0,0 +1,13 @@
+package md.cernev.quickflick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickFlickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
